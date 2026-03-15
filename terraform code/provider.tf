743c67c4d49b7ea2e56051336c883efb14adc484
aravind <ias1.email@example.com>
2026-03-15 11:39:06 +0530

    kl

--- terraform code/provider.tf
+++ terraform code/provider.tf
@@ -4,6 +4,14 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 5.0"
     }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.29"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = "~> 2.13"
+    }
     null = {
       source  = "hashicorp/null"
       version = "~> 3.0"
@@ -13,9 +21,6 @@ terraform {
 
 provider "aws" {
   region = var.aws_region
-
-
-
   default_tags {
     tags = {
       Project   = "Hackathon"
@@ -24,6 +29,16 @@ provider "aws" {
   }
 }
 
+provider "kubernetes" {
+  host                   = aws_eks_cluster.main.endpoint
+  cluster_ca_certificate = base64decode(aws_eks_cluster.main.certificate_authority[0].data)
+  token                  = data.aws_eks_cluster_auth.main.token
+}
 
-
-
+provider "helm" {
+  kubernetes {
+    host                   = aws_eks_cluster.main.endpoint
+    cluster_ca_certificate = base64decode(aws_eks_cluster.main.certificate_authority[0].data)
+    token                  = data.aws_eks_cluster_auth.main.token
+  }
+}
